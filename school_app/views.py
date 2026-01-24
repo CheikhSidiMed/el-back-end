f46805871d2f7e360a5fa7088a7040788bd312fe
Tewte Cheikh <cheikhsidimed398@gmail.com>
2026-01-24 13:59:51 +0000

    corriger

--- school_app/views.py
+++ school_app/views.py
@@ -1926,7 +1926,7 @@ def unpaid_students(request):
 
     students = Etudiant.objects.filter(
         is_inscrire=1,
-        payment_nature='mensuel'
+        payment_nature='mensuel',
         etat='inscrit', 
         is_active=True
     ).exclude(
