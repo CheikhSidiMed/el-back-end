7980cfb40b73e2e1753860244110df58d5880277
Tewte Cheikh <cheikhsidimed398@gmail.com>
2026-06-14 11:49:49 +0000

    up

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -16,6 +16,10 @@ import os
 import dj_database_url
 from decouple import config
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
