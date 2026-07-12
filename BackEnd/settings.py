f1e18e26710302db75a3c7515a5d09baf8f19a13
Tewte Cheikh <cheikhsidimed398@gmail.com>
2026-07-12 17:14:57 +0000

    up

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -92,8 +92,8 @@ WSGI_APPLICATION = 'BackEnd.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'elmanara_prod',
-        # 'NAME': 'elmanara_db',
+        # 'NAME': 'elmanara_prod',
+        'NAME': 'elmanara_db',
         'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': 'localhost',
